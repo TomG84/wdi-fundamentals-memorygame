11f428461a710f352652fd00914427782411b3ac
zoe samels <zoe@zoe-samelss-macbook.local>
2018-08-24 18:00:39 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,9 @@
+console.log("Up and running!");
+//Create variables for four cards, with two values
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king";
+//Display the value of two cards flipped by user
+console.log("User flipped " + cardThree);
+console.log("User flipped " + cardFour);
