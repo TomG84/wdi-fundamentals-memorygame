42e346ced7cb51d3d6f847db0fdbba1a0ef70431
zoe samels <zoe@zoe-samelss-macbook.local>
2018-08-28 17:24:06 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,12 +1,4 @@
 console.log("Up and running!");
-//Create variables for four cards, with two values
-/*var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
-//Display the value of two cards flipped by user
-console.log("User flipped " + cardThree);
-console.log("User flipped " + cardFour);*/
 
 //Create an array of cards with 4 strings
 var cards = ["queen", "king", "queen", "king"];
@@ -14,30 +6,27 @@ var cards = ["queen", "king", "queen", "king"];
 //Create an empty array that will hold cards in play
 var cardsInPlay = [];
 
-//Create a variable to represent first card user flipps
-var cardOne = cards[0];
-
-//Add this card to the cardsInPlay array
-cardsInPlay.push(cardOne);
-
-//Display this first card in the console
-console.log("User flipped " + cardsInPlay[0]);
-
-//Create a variable for second card user flips
-var cardTwo = cards[2];
-
-//Add this card to the cardsInPlay array
-cardsInPlay.push(cardTwo);
-
-//Display cardsInPlay array
-console.log("User flipped " + cardsInPlay[0] + " and " + cardsInPlay[1]);
-
-//Check to make sure user flipped two cards
-if (cardsInPlay.length === 2) {
-
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
-	} else {
+//Create a function to hold game logic
+var checkForMatch = function() {
+		if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You found a match");
+		} else {
 			alert("Sorry, try again.");
 		}
 	}
+}
+
+//Create a function to store all steps that should happen when user flips card
+var flipCard = function(cardId) {
+	var cardOne = cards[cardId];
+	cardsInPlay.push(cardOne);
+
+	//Display card flipped over
+	console.log("User flipped " + cards[cardId]);
+
+	checkForMatch();
+};
+
+flipCard([0]);
+flipCard([1]);
