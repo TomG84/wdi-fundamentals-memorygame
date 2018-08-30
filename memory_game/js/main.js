9ea50d9397dba9e2dabe87ee43c927e387c9b6ca
zoe samels <zoe@zoe-samelss-macbook.local>
2018-08-30 16:33:19 -0400

    added game board creation to DOM

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -29,6 +29,7 @@ var cardsInPlay = [];
 
 //Create a function to hold game logic
 var checkForMatch = function() {
+		//this.setAttribute('src', cards[cardId].cardImage);
 		if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match");
@@ -39,17 +40,27 @@ var checkForMatch = function() {
 }
 
 //Create a function to store all steps that should happen when user flips card
-var flipCard = function(cardId) {
-	var flippedCard = cards[cardId].rank;
-	cardsInPlay.push(flippedCard);
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
+	cardsInPlay.push(cards[cardId].rank);
 
 	//Display card flipped over
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-
+	this.setAttribute('src', cards[cardId].cardImage);
 	checkForMatch();
 };
 
-flipCard([0]);
-flipCard([2]);
+
+var createBoard = function() {
+	for (var i = 0; i < cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', "images/back.png");
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+};
+
+createBoard();
